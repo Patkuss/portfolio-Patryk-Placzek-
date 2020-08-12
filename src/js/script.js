5b94c8c947c22d49e30db7f90e2e21e96ad911d7
Patryk Płaczek <patryk.placzek@wp.pl>
2020-08-12 21:45:01 +0200

    Add scroll snapping and mainArea animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,34 @@
+// scroll snapping //
+
+gsap.registerPlugin(ScrollTrigger);
+
+function goToSection(i, anim) {
+  gsap.to(window, {
+    scrollTo: {y: i*innerHeight, autoKill: false},
+    duration: 1
+  });
+
+  if(anim) {
+    anim.restart();
+  }
+}
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    onEnter: () => goToSection(i)
+  });
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "bottom bottom",
+    onEnterBack: () => goToSection(i),
+  });
+});
+
+
+
+
+
 // opening //
 
 gsap.from("#svg_1", {opacity:0, duration:2, delay:2});
@@ -11,3 +42,13 @@ gsap.from(".header", {y:-80, duration:2, delay:1});
 gsap.to("#svg_0", {top:40, left:40, duration:1, delay:3.2, scale:0.2});
 gsap.from("#logo", {opacity:0, delay:4.2});
 gsap.to("#svg_0", {delay:4.4, opacity:0});
+
+gsap.from(".homeArea", {opacity:0, duration:2, delay:4.4});
+gsap.to(".LClock", {rotation:360, duration:4, repeat:-1, ease:"linear"});
+gsap.to(".SClock", {rotation:360, duration:48, repeat:-1, ease:"linear"});
+gsap.fromTo(".mouse",{x:70, duration:3}, {x:-70, duration:3, repeat:-1, yoyo: true, ease:"linear"});
+
+$(".yoda").click(function(){
+  gsap.to(".arm", {rotation:25, duration:0.15, repeat:-1, yoyo: true, transformOrigin:"bottom"});
+})
+
